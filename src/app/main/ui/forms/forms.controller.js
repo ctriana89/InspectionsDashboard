1fc96a4417a02b41c269edcf64699ff10c0ceb0a
Corali.Triana <corali.triana@sato-global.com>
2017-11-03 11:50:45 -0400

    no message

--- src/app/main/ui/forms/forms.controller.js
+++ src/app/main/ui/forms/forms.controller.js
@@ -92,32 +92,58 @@
         function showDataDialog(ev, data)
         {
             // You can do an API call here to send the form to your server
+          //var docDefinition = { content: 'This is an sample PDF printed with pdfMake' };
+          var docDefinition = {
+          content: [
+            { text: 'All Florida Inspection & Exterminating, Inc', style: 'header' },
+            'By Garry',
+            { text: 'Property Owner: '+vm.verticalStepper.step1.firstname+' '+vm.verticalStepper.step1.lastname, style: 'anotherStyle' },
+            { text: 'Address: '+vm.verticalStepper.step2.address, style: [ 'header', 'anotherStyle' ] },
+            { text: 'Buyers: '+vm.verticalStepper.step3.firstname+' '+vm.verticalStepper.step3.lastname, style: 'anotherStyle' },
+            { text: 'Date of inspection: '+vm.verticalStepper.step4.date, style: [ 'header', 'anotherStyle' ] },
 
-            // Show the sent data.. you can delete this safely.
-            $mdDialog.show({
-                controller         : function ($scope, $mdDialog, formWizardData)
-                {
-                    $scope.formWizardData = formWizardData;
-                    $scope.closeDialog = function ()
-                    {
-                        $mdDialog.hide();
-                    };
-                },
-                template           : '<md-dialog>' +
-                '  <md-dialog-content><h1>You have sent the form with the following data</h1><div><pre>{{formWizardData | json}}</pre></div></md-dialog-content>' +
-                '  <md-dialog-actions>' +
-                '    <md-button ng-click="closeDialog()" class="md-primary">' +
-                '      Close' +
-                '    </md-button>' +
-                '  </md-dialog-actions>' +
-                '</md-dialog>',
-                parent             : angular.element('body'),
-                targetEvent        : ev,
-                locals             : {
-                    formWizardData: data
-                },
-                clickOutsideToClose: true
-            });
+          ],
+
+          styles: {
+            header: {
+              fontSize: 22,
+              bold: true
+            },
+            anotherStyle: {
+              fontSize: 12,
+              italic: true,
+              alignment: 'left'
+            }
+          }
+        };
+
+          pdfMake.createPdf(docDefinition).open();
+
+            // // Show the sent data.. you can delete this safely.
+            // $mdDialog.show({
+            //     controller         : function ($scope, $mdDialog, formWizardData)
+            //     {
+            //         $scope.formWizardData = formWizardData;
+            //         $scope.closeDialog = function ()
+            //         {
+            //             $mdDialog.hide();
+            //         };
+            //     },
+            //     template           : '<md-dialog>' +
+            //     '  <md-dialog-content><h1>You have sent the form with the following data</h1><div><pre>{{formWizardData | json}}</pre></div></md-dialog-content>' +
+            //     '  <md-dialog-actions>' +
+            //     '    <md-button ng-click="closeDialog()" class="md-primary">' +
+            //     '      Close' +
+            //     '    </md-button>' +
+            //     '  </md-dialog-actions>' +
+            //     '</md-dialog>',
+            //     parent             : angular.element('body'),
+            //     targetEvent        : ev,
+            //     locals             : {
+            //         formWizardData: data
+            //     },
+            //     clickOutsideToClose: true
+            // });
         }
 
         /**
